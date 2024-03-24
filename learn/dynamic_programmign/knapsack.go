8d07dd4c3a10780da0784d63192384f9cbd1d8cc
cupid5trick <50273795+cupid5trick@users.noreply.github.com>
2024-03-24 22:53:30 +0800

    feat(learn): 背包问题之零钱兑换 https://leetcode.cn/problems/coin-change

---
+++ learn/dynamic_programmign/knapsack.go
@@ -0,0 +1,77 @@
+package main
+
+/*
+【题单】动态规划（入门/背包/状态机/划分/区间/状压/数位/树形/数据结构优化） - 力扣（LeetCode）: https://leetcode.cn/circle/discuss/tXLS3i/
+讲解：[0-1 背包 完全背包](https://leetcode.cn/link/?target=https://www.bilibili.com/video/BV16Y411v7Y6/)
+
+#### [](https://leetcode.cn/circle/discuss/tXLS3i//#§31-01-背包)§3.1 0-1 背包
+
+每个物品只能选一次。
+
+-   [2915. 和为目标值的最长子序列的长度](https://leetcode.cn/problems/length-of-the-longest-subsequence-that-sums-to-target/) 1659
+-   [顺丰 02. 小哥派件装载问题](https://leetcode.cn/contest/sf-tech/problems/cINqyA/)
+-   [416. 分割等和子集](https://leetcode.cn/problems/partition-equal-subset-sum/)
+-   [494. 目标和](https://leetcode.cn/problems/target-sum/)
+-   [2787. 将一个数字表示成幂的和的方案数](https://leetcode.cn/problems/ways-to-express-an-integer-as-sum-of-powers/) 1818
+-   [474. 一和零](https://leetcode.cn/problems/ones-and-zeroes/)（二维）
+-   [1049. 最后一块石头的重量 II](https://leetcode.cn/problems/last-stone-weight-ii/) 2092
+-   [1774. 最接近目标价格的甜点成本](https://leetcode.cn/problems/closest-dessert-cost/)
+-   [879. 盈利计划](https://leetcode.cn/problems/profitable-schemes/) 2204
+-   [3082. 求出所有子序列的能量和](https://leetcode.cn/problems/find-the-sum-of-the-power-of-all-subsequences/) ~2300
+-   [956. 最高的广告牌](https://leetcode.cn/problems/tallest-billboard/) 2381
+-   [2518. 好分区的数目](https://leetcode.cn/problems/number-of-great-partitions/) 2415
+-   [2742. 给墙壁刷油漆](https://leetcode.cn/problems/painting-the-walls/) 2425
+-   [LCP 47. 入场安检](https://leetcode.cn/problems/oPs9Bm/)
+-   [2291. 最大股票收益](https://leetcode.cn/problems/maximum-profit-from-trading-stocks/)（会员题）
+-   [2431. 最大限度地提高购买水果的口味](https://leetcode.cn/problems/maximize-total-tastiness-of-purchased-fruits/)（会员题）
+
+#### [](https://leetcode.cn/circle/discuss/tXLS3i//#§32-完全背包)§3.2 完全背包
+
+物品可以重复选，无个数限制。
+
+-   [322. 零钱兑换](https://leetcode.cn/problems/coin-change/)
+-   [518. 零钱兑换 II](https://leetcode.cn/problems/coin-change-ii/)
+-   [279. 完全平方数](https://leetcode.cn/problems/perfect-squares/)
+-   [1449. 数位成本和为目标值的最大数字](https://leetcode.cn/problems/form-largest-integer-with-digits-that-add-up-to-target/) 1927 打印方案
+
+#### [](https://leetcode.cn/circle/discuss/tXLS3i//#§33-多重背包)§3.3 多重背包
+
+物品可以重复选，有个数限制。
+
+> 注：力扣上只有求方案数的题目。
+
+-   [1155. 掷骰子等于目标和的方法数](https://leetcode.cn/problems/number-of-dice-rolls-with-target-sum/) 1654
+-   [2585. 获得分数的方法数](https://leetcode.cn/problems/number-of-ways-to-earn-points/) 1910
+-   [2902. 和带限制的子多重集合的数目](https://leetcode.cn/problems/count-of-sub-multisets-with-bounded-sum/) 2759
+
+#### [](https://leetcode.cn/circle/discuss/tXLS3i//#§34-分组背包)§3.4 分组背包
+
+同一组内的物品至多/恰好选一个。
+
+-   [1981. 最小化目标值与所选元素的差](https://leetcode.cn/problems/minimize-the-difference-between-target-and-chosen-elements/) 2010
+-   [2218. 从栈中取出 K 个硬币的最大面值和](https://leetcode.cn/problems/maximum-value-of-k-coins-from-piles/) 2158
+*/
+
+// leetcode 322. 零钱兑换 - 力扣（LeetCode）: https://leetcode.cn/problems/coin-change
+// dfs(i, j) = min(dfs(i-1, j), dfs(i-1, j-coins[i])+1), j >= coins[i]
+// dfs(0, 0) = 0, 其他 amount+1
+
+func coinChange_322(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		dp[i] = amount + 1
+	}
+
+	for _, coin := range coins {
+		for j := 1; j <= amount; j++ {
+			if j >= coin {
+				dp[j] = min(dp[j], dp[j-coin]+1)
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	}
+	return dp[amount]
+}
