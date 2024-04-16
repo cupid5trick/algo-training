aa94061dfd67dea6ed7c756d641a5b8bba347dd9
cupid5trick <50273795+cupid5trick@users.noreply.github.com>
2024-04-16 21:46:41 +0800

    feat(interview): 淘天闲鱼一面 20240416.

---
+++ interview/taotian20240416/A.java
@@ -0,0 +1,55 @@
+package interview.taotian20240416;
+
+/**
+ * 反转单链表
+ */
+public class A {
+    static class ListNode {
+        public ListNode(int i, ListNode head) {
+            val = i;
+            next = head;
+        }
+        int val;
+        ListNode next;
+    }
+
+    public static ListNode reverse(ListNode head) {
+        // ListNode dummy = new ListNode(-1, head);
+        ListNode pre = null, node = head;
+
+        while (node != null) {
+            ListNode tmp = node.next;
+            node.next = pre;
+            pre = node;
+            node = tmp;
+        }
+
+        return pre;
+        
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1, null);
+        ListNode n2 = new ListNode(2, null);
+        ListNode n3 = new ListNode(3, null);
+        ListNode n4 = new ListNode(4, null);
+        ListNode n5 = new ListNode(5, null);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        ListNode tmp = n1;
+        while (tmp != null) {
+            System.out.printf(" %d", tmp.val);
+            tmp = tmp.next;
+        }
+        ListNode res = reverse(n1);
+        while (res != null) {
+            System.out.printf(" %d", res.val);
+            res = res.next;
+        }
+
+    }
+}
+
---
+++ interview/taotian20240416/B.java
@@ -0,0 +1,34 @@
+package interview.taotian20240416;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+
+/**
+ * 一道不带优先级的括号匹配
+ */
+public class B {
+
+    public static boolean check(char a, char b) {
+        return a == '(' && b == ')' || a == '[' && b == ']' || a == '{' && b == '}';
+    }
+    
+    public static void main(String[] args) {
+        Deque<Character> stk = new LinkedList<>();
+        String input = "(}";
+        boolean ans = true;
+        for (char c : input.toCharArray()) {
+
+            if (c == '(' || c == '[' || c == '{') {
+                stk.offerLast(c);
+            } else if (!check(stk.getLast(), c)) {
+                // System.out.printf("%c %c\n", c, stk.getLast());
+                stk.pollLast();
+                ans = false;
+            } 
+        }
+
+        System.out.println(ans);
+    }
+}
